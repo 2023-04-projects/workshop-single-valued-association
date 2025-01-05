ab359423b26d87fcaae0652535ce1c838bed6706
thasmiyas39@gmail.com <Thasmiya@gmail.com>
2025-01-05 06:51:25 -0500

    Merge branch 'KHADRI-04012025-0630-PM'
    # Conflicts:
    #	src/main/java/com/khadri/jpa/entity/Location.java
    #	src/main/java/com/khadri/jpa/repository/EntityRepository.java

--- src/main/resources/SQL_Scripts/scripts.sql
+++ src/main/resources/SQL_Scripts/scripts.sql
@@ -2,3 +2,4 @@ use 2024_batch;
 create table doctor(doctorId int auto_increment primary key,doctorName varchar(100),specialty varchar(100),phonenumber Long);
 create table patient(patientId int auto_increment primary key,patientName varchar(100),phoneNumber Long,doctorId int,FOREIGN KEY(doctorId) REFERENCES doctor(doctorId));
 create table appointment(appointId INT AUTO_INCREMENT PRIMARY KEY,appointDate DATE,location varchar(10),patientId int,FOREIGN KEY(patientId) REFERENCES patient(patientId));
+ALTER TABLE patient ADD COLUMN appointId int;
